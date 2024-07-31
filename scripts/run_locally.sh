67f64433ba115641663142ea46b29eb0f2e63924
liuqiang <liuqiang1357@163.com>
2024-07-31 17:22:05 +0800

    add neox testnet configs and data

--- scripts/run_locally.sh
+++ scripts/run_locally.sh
@@ -8,7 +8,9 @@ docker compose pull \
   && docker compose up -d \
   && echo "==> $(date +%H:%M:%S) ==> Creating super-user for Safe Config Service... (may take a while)" \
   && docker compose exec cfg-web python src/manage.py createsuperuser \
-  && echo "==> $(date +%H:%M:%S) ==> Creating super-user for Safe Transaction Service... (may take a while)" \
-  && docker compose exec txs-web python manage.py createsuperuser || exit
+  && echo "==> $(date +%H:%M:%S) ==> Creating super-user for Mainnet Safe Transaction Service... (may take a while)" \
+  && docker compose exec mainnet-txs-web python manage.py createsuperuser \
+  && echo "==> $(date +%H:%M:%S) ==> Creating super-user for Testnet Safe Transaction Service... (may take a while)" \
+  && docker compose exec testnet-txs-web python manage.py createsuperuser || exit
 
 echo "==> $(date +%H:%M:%S) ==> All set! You may want to add a ChainInfo into the Config service. Please use the link below to fill its data: http://localhost:8000/cfg/admin/chains/chain/add/"
